cc2698200bfbcf079aad6934b95ba3d1e3fe6310
CopyPuxxyCat <johnnynguyenkuli218@gmail.com>
2025-06-27 23:52:17 +0700

    skill adjust

--- Assets/Scipts/Jett/JettStats.cs
+++ Assets/Scipts/Jett/JettStats.cs
@@ -10,14 +10,14 @@ public class JettStats : MonoBehaviour
     // Dash
     [System.NonSerialized] public float dashSpeed = 30f;
     [System.NonSerialized] public float dashDurationSeconds = 0.4f;
-    [System.NonSerialized] public int maxDashAttempts = 100;
+    [System.NonSerialized] public int maxDashAttempts = 3;
 
     // Smokes
-    [System.NonSerialized] public int maxSmokeAttempts = 100;
+    [System.NonSerialized] public int maxSmokeAttempts = 3;
     [System.NonSerialized] public float smokeDelaySeconds = 0.3f;
 
     // Updraft
-    [System.NonSerialized] public int maxUpdraftAttempts = 10;
+    [System.NonSerialized] public int maxUpdraftAttempts = 3;
     [System.NonSerialized] public float updraftHeight = 4.0f;
     [System.NonSerialized] public float updraftDelaySeconds = 0.2f;
 }
